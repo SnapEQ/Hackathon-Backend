1657fd85df6593ba72d0c752825c088c03bbbc80
SnapEQ <woo.biink@gmail.com>
2025-12-05 16:38:06 +0100

    Repo init

---
+++ src/test/java/com/team1/hackathonbackend/HackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.hackathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
